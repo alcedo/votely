5182668af63b889d3d1875b788331e49c1992bf8
victor <konsolidate@gmail.com>
2013-06-28 18:36:19 +0800

    _automated commit via script_

--- commitAll.sh
+++ commitAll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "pushing to the following svr: "
+echo "pushing to the following git svrs: "
 git remote
 git add . -A #-A is to commit new / deleted dirs. 
-git commit -m '_automated commit via script'
+git commit -m '_automated commit via script_'
 git push github master #git push to github
 git push origin master #git push to default remote host -> assembla
