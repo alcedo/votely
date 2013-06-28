66b04f35bc953458e63c5d7ed462ab04c836720f
victor <konsolidate@gmail.com>
2013-06-28 19:02:32 +0800

    Added skeletal code to handle vote events

--- votefor/client/views/shelfDisplay.js
+++ votefor/client/views/shelfDisplay.js
@@ -1,11 +1,19 @@
+// TEMPLATE: shelfDisplay
 Template.shelfDisplay.displayShelfTag = function () { 
 	return Session.get("shelfTag");
 };
 
+
+// TEMPLATE: showShelfItems
 Template.showShelfItems.listShelfItems = function () { 
 	return Models.GetAllShelfItem(Session.get("shelfTag"));
 };
 
+Template.showShelfItems.events = {
+
+};
+
+// TEMPLATE: genShortPoll 
 Template.genShortPoll.voteUrl = function () { 
 	return MyApp.utility.getHostName() + '/vote/';
 };
